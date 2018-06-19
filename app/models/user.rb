37a846e5b89aa51a052292c2e4429d1f24baf498
nachtmusik <to.akiyama.dev@gmail.com>
2018-06-19 14:24:19 +0900

    Relationship Model追加. switch_user Gem追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :microposts
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverses_of_relationship, source: :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+userList = []
+10.times do |i|
+  userList.push(User.create email: "example#{i}@example.com",
+                password: "testuser#{i}",
+                password_confirmation: "testuser#{i}",
+                name: "user#{i}")
+end
+
+4.times do |i|
+  user = userList[i]
+  5.times do |j|
+    Relationship.create user: user, follow: userList[j + 4]
+  end
+end
