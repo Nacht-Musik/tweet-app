2400b096be2af730b39b66327254c19219d978b4
nachtmusik <to.akiyama.dev@gmail.com>
2018-06-25 17:51:58 +0900

    自他ツイート判定追加。フォロー／アンフォロー判定追加。

--- app/controllers/top_pages_controller.rb
+++ app/controllers/top_pages_controller.rb
@@ -1,10 +1,12 @@
 class TopPagesController < ApplicationController
-  before_action :set_posts, only: [:index]
+  # before_action :set_posts, only: [:index]
   before_action :set_users, only: [:index]
 
   def index
     if user_signed_in?
+      @posts = Micropost.all
       @micropost = current_user.microposts.build
+      @follow_users = current_user.followings.ids
     end
   end
 
@@ -12,7 +14,9 @@ class TopPagesController < ApplicationController
     def set_posts
       @posts = Micropost.all
     end
+
     def set_users
       @users = User.all
     end
+
 end
